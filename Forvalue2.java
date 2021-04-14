a20ce77367e2beb04f62344b308a0365b8678f3d
ppp308 <ppp308@naver.com>
2021-04-14 19:04:03 +0900

    환전프로그램 변수 클래스 코드

---
+++ Forvalue2.java
@@ -0,0 +1,18 @@
+package defnew;
+
+public class Forvalue2 {
+	public static final int CHANGE_TYPE_USD=1;
+	public static final int CHANGE_TYPE_EURO=2;
+	public static final int CHANGE_TYPE_YEN=3;
+	public static final double USD_EXCHANGE=1134.50;
+	public static final double EURO_EXCHANGE=1332.82;
+	public static final double YEN_EXCHANGE=10.31;
+	
+	public static int BALANCE_USD = 500;
+	public static int BALANCE_EUR =500;
+	public static int BALANCE_JPY = 100;
+	
+	public static int ERR_USD =1;
+	public static int ERR_EUR =2;
+	public static int ERR_JPY =3;
+}
